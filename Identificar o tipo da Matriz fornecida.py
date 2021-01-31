3a1e736736ad6067ae50a82b53d986f82a58a845
IuryChagas25 <78304105+IuryChagas25@users.noreply.github.com>
2021-01-31 10:44:04 -0300

    Add files via upload
    Este código tem como finalidade identificar o tipo de matriz a qual o usuário forneceu, baseando-se em definições e conceitos aprendidos na disciplina de Álgebra Linear.
    fonte dos conceitos aplicados: Álgebra Linear Com Aplicações - Anton Rorres - 8a Edição.

---
+++ Identificar o tipo da Matriz fornecida.py
@@ -0,0 +1,60 @@
+import numpy as np
+#variaveis com inicialização em 0 para retirar lixo de memoria
+identidade = 0
+simetrica = 0
+antisimetrica = 0
+nula = 0
+#solicitando ao usuario os dados necessarios
+print("##### Este programa identifica a matriz digitada e informa o seu tipo! ####### \n")
+Coluna = int(input("Digita o valor da coluna da matriz: \n"))
+
+Linha = int(input("Digita o valor da linha da matriz: \n"))
+#inicializando uma matriz com lixo de memoria, porem em mesma proporção digitada pelo usuario
+M = np.empty([Linha, Coluna], dtype = float)
+#implementando os valores a matriz
+for i in range(0, Linha):
+    for j in range(0, Coluna):
+      M[i][j] = float(input("Digite o elemento [{}][{}] da matriz A: ".format(i,j)))
+
+print(" A Matriz fornecida: M \n{}".format(M))
+#comparando matriz indentidade com matriz digitada
+for l in range(0,Linha):
+  for m in range(0,Coluna):
+    I = np.eye(Coluna)
+    if M[l,m] == I[l,m]:
+      identidade += 1
+    if M[l,m] != I[l,m]:
+      identidade = 0
+      
+if identidade == Linha*Coluna:
+  resultado = 1;
+  print("\nÉ matriz identidade\n")
+#definindo matriz simetrica a partir da definição
+for s in range(0,Linha):
+  for t in range(0,Coluna):
+    if M[s,t] == M[t,s]:
+      simetrica +=1
+if simetrica == Coluna*Coluna:
+  print("\nÉ simetrica\n")
+#definindo matriz anti simetrica a partir da definição
+for a in range(0,Linha):
+  for b in range(0,Coluna):
+    if M[a,b] == - M[a,b] and M[a,a] == 0:
+      antisimetrica +=1
+if antisimetrica == Coluna:
+  print("\nÉ antisimetrica\n")
+
+#inicializando matriz nula e comparando-a com a matriz fornecida
+for c in range(0,Linha):
+  for d in range(0,Coluna):
+    Z = np.zeros((Linha, Coluna))
+    if M[c,d] == Z[c,d]:
+      nula += 1
+    if M[c,d] != Z[c,d]:
+      nula = 0
+      
+if nula == Linha*Coluna:
+  print("\nÉ matriz nula\n")
+#condição para determinar a matriz como um tipo desconhecido
+if identidade != Linha*Coluna and simetrica != Coluna*Coluna and antisimetrica != Coluna and nula != Linha*Coluna:
+  print("\nA matriz M é de um tipo desconhecido: \n{}".format(M))
